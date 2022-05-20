321da23eb43fed6897404bf8d0d61c5fd2d812e9
jamerson walderson <jamersonwalderson@gmail.com>
2022-05-20 14:36:56 -0300

    refactor: melhoria no código

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { Text, View, StyleSheet, Dimensions } from "react-native";
+import { 
+  View, 
+  StyleSheet, 
+  Dimensions, 
+} from "react-native";
 import MapView from 'react-native-maps';
 
 export default function Home() {
+
 	return(
 		<View style={{flex: 1, alignItems: 'center'}}>
-      <Text>Aqui</Text>
 			 <MapView
         style={styles.map}
 				initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: -9.648139,
+          longitude: -35.717239,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
